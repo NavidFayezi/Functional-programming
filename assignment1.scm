dd0f2242138d742a40f08acd06ea529090f977b5
NavidFayezi <navidfayezi.98@gmail.com>
2021-04-27 20:20:34 +0430

    add sum_of_scaled_rows

--- assignment1.scm
+++ assignment1.scm
@@ -22,18 +22,29 @@
         )
   )
 
-(define (scaler_vector scaler vec_a) ; scaler * vector
-        (if (eqv? (cdr vec_a) '())
-            (cons (* scaler (car vec_a)) '())
-            (cons (* scaler (car vec_a))
-              (scaler_vector scaler (cdr vec_a))
+(define (scaler_vector scaler vec) ; scaler * vector
+        (if (eqv? (cdr vec) '())
+            (cons (* scaler (car vec)) '())
+            (cons (* scaler (car vec))
+              (scaler_vector scaler (cdr vec))
               
              )
          )
   )
 
+(define (sum_of_scaled_rows vec matrix) ; sigma(vec[i] * matrix[i])
+        (if (eqv? (cdr vec) '())
+            (scaler_vector (car vec) (car matrix))
+            (add_vector (scaler_vector (car vec)(car matrix)) (sum_of_scaled_rows (cdr vec) (cdr matrix)))
+         )
+  )
 
-(display (scaler_vector (car '(2 3 4)) '(2 3 4 5)))
+
+(define matrix_a '((1 2 3) (4 5 6) (7 8 9)))
+(define matrix_b '((3 9 2) (3 5 4) (7 0 9)))
+(display (sum_of_scaled_rows (car matrix_b) matrix_a))
+;(display (sum_of_scaled_rows '(1 1 1) matrix_a))
+;(display (scaler_vector (car '(2 3 4)) '(2 3 4 5)))
 ;(display (add_vector '(1 2 3 3) '(1 3 2 3)))
 ;(display (sum '(node (node (leaf 1) (leaf 2)) (leaf 2)))) ; q1
 
